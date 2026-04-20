e4eef6e0b842f9e469041b91914498184442465c
janfdev <fauzanrizqinoor@gmail.com>
2026-04-20 16:14:16 +0700

    add named import

--- src/services/collaborations/repositories/collaboration-repositories.js
+++ src/services/collaborations/repositories/collaboration-repositories.js
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 import { nanoid } from "nanoid";
-import CacheService from "../../../cache/redis-service";
+import CacheService from "../../../cache/redis-service.js";
 
 class CollaborationRepositories {
   constructor() {
